6bfd1bb1dcb7b51f327e8b1cfb9d135c98f1b22b
Inmo Jang <inmo.jang@manchester.ac.uk>
2020-06-25 21:58:44 +0100

    Refactored

--- src/gen_obstacles.py
+++ src/gen_obstacles.py
@@ -10,20 +10,7 @@ from std_msgs.msg import Header, ColorRGBA
 import readchar
 
 
-
-def show_text_in_rviz(marker_publisher, text):
-    marker = Marker(
-                type=Marker.TEXT_VIEW_FACING,
-                id=0,
-                lifetime=rospy.Duration(1.5),
-                pose=Pose(Point(0.5, 0.5, 1.45), Quaternion(0, 0, 0, 1)),
-                scale=Vector3(0.06, 0.06, 0.06),
-                header=Header(frame_id='base_link'),
-                color=ColorRGBA(0.0, 1.0, 0.0, 0.8),
-                text=text)
-    marker_publisher.publish(marker)
-
-
+mode = 'Manual' # `RandomMove`
 
 def gen_multiple_spheres_in_rviz(radius, num_obstacles, range_obstacles, lifetime = rospy.Duration(0)):
       
@@ -70,14 +57,14 @@ def main():
   sphere_array = gen_multiple_spheres_in_rviz(radius, num_obstacles, range_obstacles)  
 
   # Add a bottom plane
-  sphere_array.markers.append(Marker(
-                  type=Marker.SPHERE,
-                  id=2001,
-                  # lifetime=rospy.Duration(1.5),
-                  pose=Pose(Point(0, 0, -0.1), Quaternion(0, 0, 0, 1)),
-                  scale=Vector3(2*100, 2*100, 2*0.05), # Note: "scale" is a diameter.
-                  header=Header(frame_id='base_link'),
-                  color=ColorRGBA(0.0, 0.0, 1.0, 0.1)) )
+#   sphere_array.markers.append(Marker(
+#                   type=Marker.SPHERE,
+#                   id=2001,
+#                   # lifetime=rospy.Duration(1.5),
+#                   pose=Pose(Point(0, 0, -0.1), Quaternion(0, 0, 0, 1)),
+#                   scale=Vector3(2*100, 2*100, 2*0.05), # Note: "scale" is a diameter.
+#                   header=Header(frame_id='base_link'),
+#                   color=ColorRGBA(0.0, 0.0, 1.0, 0.1)) )
 
   # Add a glove box
 #   box_side_half = 0.2      
@@ -94,52 +81,14 @@ def main():
   # Publish the array
   rate = rospy.Rate(10) # 10hz
   while not rospy.is_shutdown():
-    marker_array_publisher.publish(sphere_array)
-    key = readchar.readkey()
-    if key == 'w':
-        sphere_array.markers[obstacle_id].pose.position.x += pos_stride  
-    elif key == 'x':
-        sphere_array.markers[obstacle_id].pose.position.x -= pos_stride
-    elif key == 'a':
-        sphere_array.markers[obstacle_id].pose.position.y += pos_stride
-    elif key == 'd':
-        sphere_array.markers[obstacle_id].pose.position.y -= pos_stride
-    elif key == 'q':
-        sphere_array.markers[obstacle_id].pose.position.z += pos_stride
-    elif key == 'z':
-        sphere_array.markers[obstacle_id].pose.position.z -= pos_stride
-    elif key == '0':
-        obstacle_id = 0;
-        print("Selected Obstacle", obstacle_id)
-    elif key == '1':
-        obstacle_id = 1;
-        print("Selected Obstacle", obstacle_id)        
-    elif key == '2':
-        obstacle_id = 2;
-        print("Selected Obstacle", obstacle_id)        
-    elif key == '3':
-        obstacle_id = 3;
-        print("Selected Obstacle", obstacle_id)        
-    elif key == '4':
-        obstacle_id = 4;
-        print("Selected Obstacle", obstacle_id)        
-    elif key == '5':
-        obstacle_id = 5;
-        print("Selected Obstacle", obstacle_id)        
-    elif key == '6':
-        obstacle_id = 6;
-        print("Selected Obstacle", obstacle_id)        
-    elif key == '7':
-        obstacle_id = 7;
-        print("Selected Obstacle", obstacle_id)                                                                            
-    elif key == '8':
-        obstacle_id = 8;
-        print("Selected Obstacle", obstacle_id)        
-    elif key == '9':
-        obstacle_id = 9;        
-        print("Selected Obstacle", obstacle_id)        
-    elif key == '\x03':
-        break
+    if mode == 'Manual':
+        marker_array_publisher.publish(sphere_array)
+        key = readchar.readkey()
+        if key == '\x03':
+            break        
+        sphere_array = modify_sphere_array(sphere_array, key)
+
+    # elif mode = 'RandomMove':
 
     
     rate.sleep()
@@ -149,7 +98,54 @@ def main():
   marker_array_publisher.publish(sphere_array)
   marker_array_publisher.publish(MarkerArray())
  
-  
+
+def modify_sphere_array(sphere_array, key):
+        
+        if key == 'w':
+            sphere_array.markers[obstacle_id].pose.position.x += pos_stride  
+        elif key == 'x':
+            sphere_array.markers[obstacle_id].pose.position.x -= pos_stride
+        elif key == 'a':
+            sphere_array.markers[obstacle_id].pose.position.y += pos_stride
+        elif key == 'd':
+            sphere_array.markers[obstacle_id].pose.position.y -= pos_stride
+        elif key == 'q':
+            sphere_array.markers[obstacle_id].pose.position.z += pos_stride
+        elif key == 'z':
+            sphere_array.markers[obstacle_id].pose.position.z -= pos_stride
+        elif key == '0':
+            obstacle_id = 0;
+            print("Selected Obstacle", obstacle_id)
+        elif key == '1':
+            obstacle_id = 1;
+            print("Selected Obstacle", obstacle_id)        
+        elif key == '2':
+            obstacle_id = 2;
+            print("Selected Obstacle", obstacle_id)        
+        elif key == '3':
+            obstacle_id = 3;
+            print("Selected Obstacle", obstacle_id)        
+        elif key == '4':
+            obstacle_id = 4;
+            print("Selected Obstacle", obstacle_id)        
+        elif key == '5':
+            obstacle_id = 5;
+            print("Selected Obstacle", obstacle_id)        
+        elif key == '6':
+            obstacle_id = 6;
+            print("Selected Obstacle", obstacle_id)        
+        elif key == '7':
+            obstacle_id = 7;
+            print("Selected Obstacle", obstacle_id)                                                                            
+        elif key == '8':
+            obstacle_id = 8;
+            print("Selected Obstacle", obstacle_id)        
+        elif key == '9':
+            obstacle_id = 9;        
+            print("Selected Obstacle", obstacle_id)        
+
+
+        return sphere_array
 
 if __name__ == '__main__':
   main()
